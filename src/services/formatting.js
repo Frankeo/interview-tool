38429bbc99727bb21f59e48e6fe8da5daf4f6607
Frankeo <ingfrancisco.moreno1@gmail.com>
2020-11-28 01:20:29 +0000

    Adding TODO for delete custom table logic

--- src/services/formatting.js
+++ src/services/formatting.js
@@ -5,6 +5,13 @@ const chalk = require('chalk');
 const logInfo = (text) => signale.info(chalk.bold(text));
 const logError = (text) => signale.fatal(text);
 const logComplete = (text, declaration) => signale.complete({prefix: ' ', message: chalk.bold(text), suffix: chalk.grey(`(${declaration})`) });
+
+// TODO: Avoid custom table formatting
+/* BODY: Instead of this custom logic try with:
+[console-table-printer](https://github.com/ayonious/console-table-printer) OR
+[cli-table3](https://github.com/cli-table/cli-table3) OR
+[Native node console](https://nodejs.org/api/console.html#console_console_table_tabulardata_properties)
+ */
 const logResultTable = (list) => {
   list.forEach((row) => {
     for (const elem in row) {
