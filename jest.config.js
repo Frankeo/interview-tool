be51bdfc31b6005ffb64f28f69aef21b7913636f
Frankeo <ingfrancisco.moreno1@gmail.com>
2020-12-01 00:47:01 +0000

    exclude database from test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ["src/**/*.js"],
-    coveragePathIgnorePatterns: ["<rootDir>/src/input", "<rootDir>/src/output"],
+    coveragePathIgnorePatterns: ["<rootDir>/src/input", "<rootDir>/src/output", "<rootDir>/src/db"],
     coverageReporters: ["json-summary", "text", "lcov"],
     modulePathIgnorePatterns: ["<rootDir>/src/input", "<rootDir>/src/output"]
 };
