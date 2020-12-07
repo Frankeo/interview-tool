b27305b2ed995320bd13bb2421373d6518a0848c
Restyled.io <commits@restyled.io>
2020-12-07 19:02:16 +0000

    Restyled by clang-format

--- src/list-module/command.test.js
+++ src/list-module/command.test.js
@@ -1,7 +1,7 @@
-const { handler } = require("./command");
-const { getAllChallenges } = require("../db/db-management");
+const {handler} = require("./command");
+const {getAllChallenges} = require("../db/db-management");
 jest.mock("../db/db-management");
-const { logInfo, logTable, logError } = require("../services/formatting");
+const {logInfo, logTable, logError} = require("../services/formatting");
 jest.mock("../services/formatting");
 
 describe("testing handler", () => {
@@ -11,28 +11,29 @@ describe("testing handler", () => {
     logInfo.mockReset();
   });
 
-  test("should call logInfo if getAllChallenges not return any project", async () => {
-    getAllChallenges.mockImplementation(() => undefined);
-    await handler();
-    expect(logInfo).toHaveBeenCalledTimes(1);
-    expect(logInfo).toHaveBeenCalledWith("Not project to show, please add one");
-  });
+  test("should call logInfo if getAllChallenges not return any project",
+       async () => {
+         getAllChallenges.mockImplementation(() => undefined);
+         await handler();
+         expect(logInfo).toHaveBeenCalledTimes(1);
+         expect(logInfo).toHaveBeenCalledWith(
+             "Not project to show, please add one");
+       });
 
-  test("should call logInfo if getAllChallenges return any project", async () => {
-    const projects = ["project"];
-    getAllChallenges.mockImplementation(() => projects);
-    await handler();
-    expect(logInfo).toHaveBeenCalledTimes(1);
-    expect(logInfo).toHaveBeenCalledWith("Interview challenges");
-    expect(logTable).toHaveBeenCalledTimes(1);
-    expect(logTable).toHaveBeenCalledWith(projects);
-  });
+  test("should call logInfo if getAllChallenges return any project",
+       async () => {
+         const projects = [ "project" ];
+         getAllChallenges.mockImplementation(() => projects);
+         await handler();
+         expect(logInfo).toHaveBeenCalledTimes(1);
+         expect(logInfo).toHaveBeenCalledWith("Interview challenges");
+         expect(logTable).toHaveBeenCalledTimes(1);
+         expect(logTable).toHaveBeenCalledWith(projects);
+       });
 
   test("should call logError if getAllChallenges throw an Error", async () => {
     const error = "error";
-    getAllChallenges.mockImplementation(() => {
-      throw new Error(error);
-    });
+    getAllChallenges.mockImplementation(() => { throw new Error(error); });
     await handler();
     expect(logError).toHaveBeenCalledTimes(1);
     expect(logError).toHaveBeenCalledWith(error);
