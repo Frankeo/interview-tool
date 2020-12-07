cd860e00e1c58c7f72f35f8cd8c47788bd6a4987
Restyled.io <commits@restyled.io>
2020-12-07 14:24:09 +0000

    Restyled by clang-format

--- src/services/formatting.js
+++ src/services/formatting.js
@@ -4,12 +4,11 @@ const consolePrinter = require("console-table-printer");
 
 const logInfo = (text) => signale.info(chalk.bold(text));
 const logError = (text) => signale.fatal(text);
-const logComplete = (text, declaration) =>
-  signale.complete({
-    prefix: " ",
-    message: chalk.bold(text),
-    suffix: chalk.grey(`(${declaration})`),
-  });
+const logComplete = (text, declaration) => signale.complete({
+  prefix : " ",
+  message : chalk.bold(text),
+  suffix : chalk.grey(`(${declaration})`),
+});
 
 const logTable = (list) => {
   const table = new consolePrinter.Table();
